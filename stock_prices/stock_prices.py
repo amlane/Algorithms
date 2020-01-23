f2e33b56438e38d7ebd4e9e8042c4db997f9aea9
amlane <twelvexstring@gmail.com>
2020-01-22 19:09:09 -0600

    found O(n) solution for find_max_profit

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -5,19 +5,36 @@ import argparse
 # this solution has a runtime of O(n^2)
 
 
+# def find_max_profit(prices):
+#     result = float('-inf')
+# # starting at the end of the list, compare each number
+#     for i in range(len(prices) - 1, -1, -1):
+#         for j in range(len(prices) - 1, -1, -1):
+#             # if i > j, subtract arr[i] - arr[j]
+#             if i > j:
+#               # if the value of i > j do this:
+#                 if result < prices[i] - prices[j]:
+#                     # if the result is greater than the current value of temp var, store the result in a temp var
+#                     result = prices[i] - prices[j]
+#               # else if the value of j > i
+# # once index == 0 return temp var
+#     return result
+
+# O(n) solution
+
 def find_max_profit(prices):
+
+    # find the max number in the list (don't include first number)
+    largest = max(prices[1:])
+    newList = prices[:prices.index(largest)]
     result = float('-inf')
-# starting at the end of the list, compare each number
-    for i in range(len(prices) - 1, -1, -1):
-        for j in range(len(prices) - 1, -1, -1):
-            # if i > j, subtract arr[i] - arr[j]
-            if i > j:
-              # if the value of i > j do this:
-                if result < prices[i] - prices[j]:
-                    # if the result is greater than the current value of temp var, store the result in a temp var
-                    result = prices[i] - prices[j]
-              # else if the value of j > i
-# once index == 0 return temp var
+
+    # find the lowest number to the left of that list
+    for i in range(0, len(newList)):
+        # print(newList[i])
+        if result < largest - newList[i]:
+            result = largest - newList[i]
+
     return result
 
 
